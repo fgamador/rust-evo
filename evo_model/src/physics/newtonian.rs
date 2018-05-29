d5c6a96baa6b53156c1fea80666457ea52852bc1
Franz Amador <franzamador@gmail.com>
2018-05-28 17:06:45 -0700

    Add velocity

--- evo_model/src/physics/newtonian.rs
+++ evo_model/src/physics/newtonian.rs
@@ -1,12 +1,13 @@
 pub trait Newtonian {
     fn x(&self) -> f64;
+    fn vx(&self) -> f64;
     //    fn add_force(&self, fx: f64);
     fn step(&mut self);
 }
 
 pub struct NewtonianState {
     pub x: f64,
-//    pub vx: f64,
+    pub vx: f64,
 //    pub mass: f64,
 }
 
@@ -15,8 +16,12 @@ impl Newtonian for NewtonianState {
         self.x
     }
 
+    fn vx(&self) -> f64 {
+        self.vx
+    }
+
     fn step(&mut self) {
-        // TODO
+        self.x += self.vx;
     }
 }
 
@@ -33,15 +38,28 @@ mod tests {
             self.state.x()
         }
 
+        fn vx(&self) -> f64 {
+            self.state.vx()
+        }
+
         fn step(&mut self) {
             self.state.step();
         }
     }
 
     #[test]
-    fn no_forces_no_acceleration() {
-        let mut body = SimpleNewtonian { state: NewtonianState { x: 0.0 } };
+    fn stationary() {
+        let mut body = SimpleNewtonian { state: NewtonianState { x: 0.0, vx: 0.0 } };
         body.step();
         assert_eq!(0.0, body.x());
+        assert_eq!(0.0, body.vx());
+    }
+
+    #[test]
+    fn coasting() {
+        let mut body = SimpleNewtonian { state: NewtonianState { x: 0.0, vx: 1.0 } };
+        body.step();
+        assert_eq!(1.0, body.x());
+        assert_eq!(1.0, body.vx());
     }
 }

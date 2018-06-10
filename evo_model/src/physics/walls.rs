dddcc76d2d9cb71293e4cad25a317d253a29a2c5
Franz Amador <franzamador@gmail.com>
2018-06-09 23:10:18 -0700

    Detect other corner wall overlap

--- evo_model/src/physics/walls.rs
+++ evo_model/src/physics/walls.rs
@@ -65,17 +65,17 @@ impl Walls {
             let mut overlap_x = 0.0;
             let mut overlap_y = 0.0;
             let circle_box = circle.to_bounding_box();
-            if circle_box.max_corner().x() > self.max_corner.x() {
-                overlap_x += self.max_corner.x() - circle_box.max_corner().x();
-            }
-            if circle_box.max_corner().y() > self.max_corner.y() {
-                overlap_y += self.max_corner.y() - circle_box.max_corner().y();
-            }
             if circle_box.min_corner().x() < self.min_corner.x() {
                 overlap_x += self.min_corner.x() - circle_box.min_corner().x();
             }
             if circle_box.min_corner().y() < self.min_corner.y() {
                 overlap_y += self.min_corner.y() - circle_box.min_corner().y();
+            }
+            if circle_box.max_corner().x() > self.max_corner.x() {
+                overlap_x += self.max_corner.x() - circle_box.max_corner().x();
+            }
+            if circle_box.max_corner().y() > self.max_corner.y() {
+                overlap_y += self.max_corner.y() - circle_box.max_corner().y();
             }
             if overlap_x != 0.0 || overlap_y != 0.0 {
                 overlaps.push(Overlap::new(circle, Displacement::new(overlap_x, overlap_y)));
@@ -103,6 +103,15 @@ mod tests {
         let circles = vec![Circle::new(Position::new(8.5, 0.75), Length::new(1.0))];
         let overlaps = subject.find_overlaps(&circles);
         assert!(overlaps.is_empty());
+    }
+
+    #[test]
+    fn min_corner_overlap() {
+        let subject = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
+        let circles = vec![Circle::new(Position::new(-9.5, -4.25), Length::new(1.0))];
+        let overlaps = subject.find_overlaps(&circles);
+        assert_eq!(1, overlaps.len());
+        assert_eq!(Overlap::new(&circles[0], Displacement::new(0.5, 0.25)), overlaps[0]);
     }
 
     #[test]

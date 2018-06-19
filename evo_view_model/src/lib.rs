00a31fc309abbfb45cea1419b6598650c1726014
Franz Amador <franzamador@gmail.com>
2018-06-18 19:39:16 -0700

    Passing test

--- evo_view_model/src/lib.rs
+++ evo_view_model/src/lib.rs
@@ -1,17 +1,31 @@
-pub struct ViewModel {}
+type BoxedCallback = Box<Fn(&mut bool)>;
 
-//type BoxedCallback = Box<FnMut(&ViewModel)>;
+pub struct ViewModel {
+    pub updated: bool,
+    render_done_listeners: Vec<BoxedCallback>,
+    //update_done_listeners: Vec<BoxedCallback>,
+}
 
 impl ViewModel {
     pub fn new() -> ViewModel {
-        ViewModel {}
+        ViewModel {
+            updated: false,
+            render_done_listeners: Vec::new(),
+            //update_done_listeners: Vec::new(),
+        }
     }
 
-    pub fn add_render_done_listener<C>(&mut self, _listener: C)
-        where C: FnMut(&ViewModel)
-    {}
+    pub fn add_render_done_listener<T>(&mut self, listener: T)
+        where T: Fn(&mut bool) + 'static
+    {
+        self.render_done_listeners.push(Box::new(listener));
+    }
 
-    pub fn render_done(&mut self) {}
+    pub fn render_done(&mut self) {
+        for listener in &self.render_done_listeners {
+            listener(&mut self.updated);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -21,9 +35,8 @@ mod tests {
     #[test]
     fn render_done_callback() {
         let mut view_model = ViewModel::new();
-        let mut listener_notified = false;
-        view_model.add_render_done_listener(|_view_model| { listener_notified = true });
+        view_model.add_render_done_listener(|updated| { *updated = true });
         view_model.render_done();
-//        assert!(listener_notified);
+        assert!(view_model.updated);
     }
 }

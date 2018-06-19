00abf541f7804e0b634c5a2ff693582a575720a3
Franz Amador <franzamador@gmail.com>
2018-06-18 19:11:17 -0700

    Add register-callback fn

--- evo_view_model/src/lib.rs
+++ evo_view_model/src/lib.rs
@@ -1,9 +1,15 @@
 pub struct ViewModel {}
 
+type BoxedCallback = Box<Fn(&ViewModel)>;
+
 impl ViewModel {
     pub fn new() -> ViewModel {
         ViewModel {}
     }
+
+    pub fn add_render_done_listener<C>(&mut self, listener: C)
+        where C: Fn(&ViewModel)
+    {}
 }
 
 #[cfg(test)]
@@ -13,6 +19,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut view_model = ViewModel::new();
+        view_model.add_render_done_listener(|view_model| {});
         assert_eq!(2 + 2, 4);
     }
 }

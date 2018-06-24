3b035e136d3ec4b49fa19533e99aaf18b6a0d32a
Franz Amador <franzamador@gmail.com>
2018-06-24 11:36:52 -0700

    r remove dead code

--- evo_view_model/src/lib.rs
+++ evo_view_model/src/lib.rs
@@ -2,8 +2,6 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::Rc;
 
-type BoxedCallback = Rc<Fn(&mut ViewModel) -> ()>;
-
 #[derive(Clone, Copy, Eq, Hash, PartialEq)]
 pub enum Event {
     Rendered,
@@ -11,84 +9,15 @@ pub enum Event {
 }
 
 pub struct ViewModel {
-    //    event_manager: EventManager<Event, ViewModel>,
     pub updated: bool,
     pub rendered: bool,
-    events: Vec<Event>,
-    listeners: HashMap<Event, Vec<BoxedCallback>>,
 }
 
 impl ViewModel {
     pub fn new() -> ViewModel {
         ViewModel {
-//            event_manager: EventManager::new(),
             updated: false,
             rendered: false,
-            events: Vec::new(),
-            listeners: HashMap::new(),
-        }
-    }
-
-    pub fn add_render_done_listener<T>(&mut self, listener: T)
-        where T: Fn(&mut ViewModel) + 'static
-    {
-//        self.event_manager.add_listener(Event::Rendered, listener);
-        self.add_listener(Event::Rendered, listener);
-    }
-
-    pub fn add_update_done_listener<T>(&mut self, listener: T)
-        where T: Fn(&mut ViewModel) + 'static
-    {
-//        self.event_manager.add_listener(Event::Updated, listener);
-        self.add_listener(Event::Updated, listener);
-    }
-
-    pub fn render_done(&mut self) {
-//        self.event_manager.add_event(Event::Rendered);
-        self.add_event(Event::Rendered);
-    }
-
-    pub fn update_done(&mut self) {
-//        self.event_manager.add_event(Event::Updated);
-        self.add_event(Event::Updated);
-    }
-
-    pub fn fire_events(&mut self) {
-        //self.event_manager.fire_events(self);
-        while !self.events.is_empty() {
-            let events = self.events.clone();
-            self.events.clear();
-            for event in events {
-                self.fire_event(event)
-            }
-        }
-    }
-}
-
-impl ViewModel {
-    pub fn add_listener<T>(&mut self, event: Event, listener: T)
-        where T: Fn(&mut ViewModel) + 'static
-    {
-        self.listeners.entry(event).or_insert(Vec::new()).push(Rc::new(listener));
-    }
-
-    pub fn add_event(&mut self, event: Event)
-    {
-        self.events.push(event);
-    }
-
-    fn fire_event(&mut self, event: Event) {
-        let listeners = self.clone_listeners(event);
-        ViewModel::notify_listeners(self, listeners);
-    }
-
-    fn clone_listeners(&self, event: Event) -> Vec<BoxedCallback> {
-        self.listeners.get(&event).unwrap().clone()
-    }
-
-    fn notify_listeners(source: &mut ViewModel, listeners: Vec<BoxedCallback>) {
-        for listener in listeners {
-            listener(source);
         }
     }
 }

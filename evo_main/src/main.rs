ab1acc37523497c335fca4eccc9df0ad01d28cc1
Franz Amador <franzamador@gmail.com>
2018-06-24 22:12:12 -0700

    wiring

--- evo_main/src/main.rs
+++ evo_main/src/main.rs
@@ -1,4 +1,6 @@
 extern crate evo_conrod;
+extern crate evo_model;
+extern crate evo_view_model;
 
 fn main() {
     evo_conrod::main();
--- evo_view_model/src/lib.rs
+++ evo_view_model/src/lib.rs
@@ -1,1 +1,11 @@
 pub mod events;
+
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub enum Event {
+    Rendered,
+    Updated,
+}
+
+pub struct ViewModel {
+    pub updated: bool,
+}

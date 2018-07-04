b42db85173bc06feb8083d89df76dcbd5288531c
Franz Amador <franzamador@gmail.com>
2018-07-04 10:25:51 -0700

    crlf -> lf; add to test

--- evo_model/src/physics/ball.rs
+++ evo_model/src/physics/ball.rs
@@ -1,5 +1,6 @@
 use physics::newtonian;
 use physics::quantities::*;
+use physics::walls::Walls;
 
 struct Ball {
     state: newtonian::State,
@@ -40,6 +41,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn wall_corner_overlap_adds_force() {
+        let subject = Ball::new(Mass::new(2.0), Position::new(-9.5, 1.75), Velocity::new(1.0, 2.0));
+        let walls = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
+//        let circles = vec![Circle::new(Position::new(-9.5, -4.25), Length::new(1.0))];
+//        let overlaps = subject.find_overlaps(&circles);
+//        assert_eq!(1, overlaps.len());
+//        assert_eq!(Overlap::new(&circles[0], Displacement::new(0.5, 0.25)), overlaps[0]);
     }
 }

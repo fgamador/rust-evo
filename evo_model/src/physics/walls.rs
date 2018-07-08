4f247b39c0a2a0f8c31b159056eaccaed0093e69
Franz Amador <franzamador@gmail.com>
2018-07-08 12:03:47 -0700

    r: rename

--- evo_model/src/physics/walls.rs
+++ evo_model/src/physics/walls.rs
@@ -1,14 +1,14 @@
 use physics::quantities::*;
 
 #[derive(Debug, PartialEq)]
-pub struct Circle {
+pub struct SimpleCircle {
     pub center: Position,
     pub radius: Length,
 }
 
-impl Circle {
-    pub fn new(center: Position, radius: Length) -> Circle {
-        Circle { center, radius }
+impl SimpleCircle {
+    pub fn new(center: Position, radius: Length) -> SimpleCircle {
+        SimpleCircle { center, radius }
     }
 
     pub fn radius(&self) -> Length {
@@ -49,12 +49,12 @@ impl BoundingBox {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Overlap<'a> {
-    circle: &'a Circle,
+    circle: &'a SimpleCircle,
     incursion: Displacement,
 }
 
 impl<'a> Overlap<'a> {
-    pub fn new(circle: &Circle, overlap: Displacement) -> Overlap {
+    pub fn new(circle: &SimpleCircle, overlap: Displacement) -> Overlap {
         Overlap { circle, incursion: overlap }
     }
 }
@@ -69,7 +69,7 @@ impl Walls {
         Walls { min_corner, max_corner }
     }
 
-    pub fn find_overlaps<'a>(&self, circles: &'a Vec<Circle>) -> Vec<Overlap<'a>> {
+    pub fn find_overlaps<'a>(&self, circles: &'a Vec<SimpleCircle>) -> Vec<Overlap<'a>> {
         let mut overlaps = vec![];
         let zero = Displacement::new(0.0, 0.0);
         for ref circle in circles {
@@ -91,7 +91,7 @@ mod tests {
 
     #[test]
     fn circle_bounding_box() {
-        let subject = Circle::new(Position::new(-0.5, 0.5), Length::new(1.0));
+        let subject = SimpleCircle::new(Position::new(-0.5, 0.5), Length::new(1.0));
         assert_eq!(BoundingBox::new(Position::new(-1.5, -0.5),
                                     Position::new(0.5, 1.5)),
                    subject.to_bounding_box());
@@ -100,7 +100,7 @@ mod tests {
     #[test]
     fn no_overlaps() {
         let subject = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
-        let circles = vec![Circle::new(Position::new(8.5, 0.75), Length::new(1.0))];
+        let circles = vec![SimpleCircle::new(Position::new(8.5, 0.75), Length::new(1.0))];
         let overlaps = subject.find_overlaps(&circles);
         assert!(overlaps.is_empty());
     }
@@ -108,7 +108,7 @@ mod tests {
     #[test]
     fn min_corner_overlap() {
         let subject = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
-        let circles = vec![Circle::new(Position::new(-9.5, -4.25), Length::new(1.0))];
+        let circles = vec![SimpleCircle::new(Position::new(-9.5, -4.25), Length::new(1.0))];
         let overlaps = subject.find_overlaps(&circles);
         assert_eq!(1, overlaps.len());
         assert_eq!(Overlap::new(&circles[0], Displacement::new(0.5, 0.25)), overlaps[0]);
@@ -117,7 +117,7 @@ mod tests {
     #[test]
     fn max_corner_overlap() {
         let subject = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
-        let circles = vec![Circle::new(Position::new(9.5, 1.75), Length::new(1.0))];
+        let circles = vec![SimpleCircle::new(Position::new(9.5, 1.75), Length::new(1.0))];
         let overlaps = subject.find_overlaps(&circles);
         assert_eq!(1, overlaps.len());
         assert_eq!(Overlap::new(&circles[0], Displacement::new(-0.5, -0.75)), overlaps[0]);

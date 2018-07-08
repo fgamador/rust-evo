e7d7e8d068d10d105b66b2d17c2cff4079aaddd3
Franz Amador <franzamador@gmail.com>
2018-07-08 11:51:30 -0700

    Add radius to Ball

--- evo_model/src/physics/ball.rs
+++ evo_model/src/physics/ball.rs
@@ -3,13 +3,15 @@ use physics::quantities::*;
 use physics::walls::Walls;
 
 struct Ball {
+    radius: Length,
     state: newtonian::State,
 }
 
 impl Ball {
-    fn new(mass: Mass, position: Position, velocity: Velocity) -> Ball {
+    fn new(radius: Length, mass: Mass, position: Position, velocity: Velocity) -> Ball {
         Ball {
-            state: newtonian::State::new(mass, position, velocity)
+            radius,
+            state: newtonian::State::new(mass, position, velocity),
         }
     }
 
@@ -42,10 +44,10 @@ mod tests {
 
     #[test]
     fn wall_corner_overlap_adds_force() {
-        let subject = Ball::new(Mass::new(2.0), Position::new(-9.5, 1.75), Velocity::new(1.0, 2.0));
+        let subject = Ball::new(Length::new(1.0), Mass::new(2.0), Position::new(-9.5, 1.75), Velocity::new(1.0, 2.0));
         let walls = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
-//        let circles = vec![Circle::new(Position::new(-9.5, -4.25), Length::new(1.0))];
-//        let overlaps = subject.find_overlaps(&circles);
+        let circles = vec![subject];
+//        let overlaps = walls.find_overlaps(&circles);
 //        assert_eq!(1, overlaps.len());
 //        assert_eq!(Overlap::new(&circles[0], Displacement::new(0.5, 0.25)), overlaps[0]);
     }

7d97e424f29f2cf9b2751778972812711a845c3a
Franz Amador <franzamador@gmail.com>
2018-07-08 17:02:14 -0700

    r format

--- evo_model/src/physics/newtonian.rs
+++ evo_model/src/physics/newtonian.rs
@@ -67,7 +67,8 @@ mod tests {
 
     #[test]
     fn coasting() {
-        let mut subject = SimpleBody::new(Mass::new(2.0), Position::new(-1.0, 1.5), Velocity::new(1.0, 2.0));
+        let mut subject = SimpleBody::new(
+            Mass::new(2.0), Position::new(-1.0, 1.5), Velocity::new(1.0, 2.0));
         subject.move_for(Duration::new(0.5));
         assert_eq!(Position::new(-0.5, 2.5), subject.position());
         assert_eq!(Velocity::new(1.0, 2.0), subject.velocity());
@@ -75,7 +76,8 @@ mod tests {
 
     #[test]
     fn kicked() {
-        let mut subject = SimpleBody::new(Mass::new(2.0), Position::new(-1.0, 2.0), Velocity::new(1.0, -1.0));
+        let mut subject = SimpleBody::new(
+            Mass::new(2.0), Position::new(-1.0, 2.0), Velocity::new(1.0, -1.0));
         subject.kick(Impulse::new(0.5, 0.5));
         assert_eq!(Position::new(-1.0, 2.0), subject.position());
         assert_eq!(Velocity::new(1.25, -0.75), subject.velocity());

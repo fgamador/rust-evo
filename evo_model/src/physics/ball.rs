4c865cbbfef9fa994206b905e581ac5392b04ce7
Franz Amador <franzamador@gmail.com>
2018-07-14 10:12:25 -0700

    r inline var

--- evo_model/src/physics/ball.rs
+++ evo_model/src/physics/ball.rs
@@ -91,9 +91,8 @@ mod tests {
     #[test]
     fn walls_add_overlap() {
         let walls = Walls::new(Position::new(-10.0, -5.0), Position::new(10.0, 2.0));
-        let ball = Ball::new(Length::new(1.0), Mass::new(2.0),
-                             Position::new(-9.5, 1.75), Velocity::new(1.0, 2.0));
-        let mut balls = vec![ball];
+        let mut balls = vec![Ball::new(Length::new(1.0), Mass::new(2.0),
+                                       Position::new(-9.5, 1.75), Velocity::new(1.0, 2.0))];
         walls.find_overlaps(&mut balls, |ball, overlap| {
             ball.environment().add_overlap(overlap);
         });

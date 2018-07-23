1e46e079577cfd21536a5157fa260394a12ec60c
Franz Amador <franzamador@gmail.com>
2018-07-22 19:58:37 -0700

    t rectangles_that_overlap

--- evo_model/src/physics/shapes.rs
+++ evo_model/src/physics/shapes.rs
@@ -50,6 +50,14 @@ impl Rectangle {
     pub fn max_corner(&self) -> Position {
         self.max_corner
     }
+
+    pub fn overlaps(&self, other: Rectangle) -> bool {
+        let self_x_range = FloatRange::new(self.min_corner.x(), self.max_corner.x());
+        let self_y_range = FloatRange::new(self.min_corner.y(), self.max_corner.y());
+        let other_x_range = FloatRange::new(other.min_corner.x(), other.max_corner.x());
+        let other_y_range = FloatRange::new(other.min_corner.y(), other.max_corner.y());
+        self_x_range.overlaps(other_x_range) && self_y_range.overlaps(other_y_range)
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -102,18 +110,32 @@ mod tests {
         assert!(!FloatRange::new(2.1, 3.0).overlaps(FloatRange::new(1.0, 2.0)));
     }
 
-//    #[test]
-//    fn rectangles_that_overlap() {
-//        let rect1 = Rectangle::new(Position::new(0.0, 0.0),
-//                                   Position::new(0.0, 0.0));
-//        let rect2 = Rectangle::new(Position::new(0.0, 0.0),
-//                                   Position::new(0.0, 0.0));
-//
-//        let subject = SimpleCircle::new(Position::new(-0.5, 0.5), Length::new(1.0));
-//        assert_eq!(Rectangle::new(Position::new(-1.5, -0.5),
-//                                  Position::new(0.5, 1.5)),
-//                   subject.to_bounding_box());
-//    }
+    #[test]
+    fn rectangles_with_only_x_overlap() {
+        let rect1 = Rectangle::new(Position::new(0.0, 0.0),
+                                   Position::new(1.5, 1.0));
+        let rect2 = Rectangle::new(Position::new(1.0, 1.0),
+                                   Position::new(2.0, 2.0));
+        assert!(!rect1.overlaps(rect2));
+    }
+
+    #[test]
+    fn rectangles_with_only_y_overlap() {
+        let rect1 = Rectangle::new(Position::new(0.0, 0.0),
+                                   Position::new(1.0, 1.5));
+        let rect2 = Rectangle::new(Position::new(1.0, 1.0),
+                                   Position::new(2.0, 2.0));
+        assert!(!rect1.overlaps(rect2));
+    }
+
+    #[test]
+    fn rectangles_that_overlap() {
+        let rect1 = Rectangle::new(Position::new(0.0, 0.0),
+                                   Position::new(1.5, 1.5));
+        let rect2 = Rectangle::new(Position::new(1.0, 1.0),
+                                   Position::new(2.0, 2.0));
+        assert!(rect1.overlaps(rect2));
+    }
 
     #[derive(Clone, Copy, Debug, PartialEq)]
     pub struct SimpleCircle {

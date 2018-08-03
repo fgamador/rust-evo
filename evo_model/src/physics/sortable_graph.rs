5d4593632ab7fa04ce8d8a51051dda68c17ab041
Franz Amador <franzamador@gmail.com>
2018-08-02 21:04:33 -0700

    r rename field

--- evo_model/src/physics/sortable_graph.rs
+++ evo_model/src/physics/sortable_graph.rs
@@ -2,7 +2,7 @@ use physics::bond::*;
 
 #[derive(Debug)]
 pub struct SortableGraph<T> {
-    pub balls: Vec<T>,
+    pub nodes: Vec<T>,
     pub indexes: Vec<usize>,
     bonds: Vec<Bond>,
 }
@@ -10,15 +10,15 @@ pub struct SortableGraph<T> {
 impl<T> SortableGraph<T> {
     pub fn new() -> Self {
         SortableGraph {
-            balls: vec![],
+            nodes: vec![],
             indexes: vec![],
             bonds: vec![],
         }
     }
 
     pub fn add_ball(&mut self, ball: T) {
-        self.indexes.push(self.balls.len());
-        self.balls.push(ball);
+        self.indexes.push(self.nodes.len());
+        self.nodes.push(ball);
     }
 
     pub fn add_bond(&mut self, bond: Bond) {
@@ -26,10 +26,10 @@ impl<T> SortableGraph<T> {
     }
 
     pub fn balls(&self) -> &[T] {
-        &self.balls
+        &self.nodes
     }
 
     pub fn balls_mut(&mut self) -> &mut [T] {
-        &mut self.balls
+        &mut self.nodes
     }
 }

c5eb2a268def9cb08f5e7bc2b374ed82c1515212
Franz Amador <franzamador@gmail.com>
2018-08-04 10:18:57 -0700

    comment

--- evo_model/src/physics/overlap.rs
+++ evo_model/src/physics/overlap.rs
@@ -82,12 +82,12 @@ pub fn find_graph_pair_overlaps<'a, C, E>(graph: &'a mut SortableGraph<C, E>, on
 }
 
 fn sort_by_min_x<C: Circle, E>(graph: &mut SortableGraph<C, E>) {
-    // TODO convert this to insertion sort
     sort(graph, cmp_by_min_x);
 }
 
 fn sort<N, E>(graph: &mut SortableGraph<N, E>, cmp: fn(&N, &N) -> Ordering) {
     let nodes = &mut graph.nodes;
+    // TODO convert this to insertion sort
     graph.node_indexes.sort_unstable_by(|i1, i2| cmp(&nodes[*i1], &nodes[*i2]));
 }
 

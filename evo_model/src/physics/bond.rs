c343aa04297b4d3b7a06a72212e1438aca08d569
Franz Amador <franzamador@gmail.com>
2018-08-05 21:39:41 -0700

    unfinished test

--- evo_model/src/physics/bond.rs
+++ evo_model/src/physics/bond.rs
@@ -49,32 +49,69 @@ mod tests {
 
     #[test]
     fn bond_calculates_strain() {
-        let ball1 = Ball::new(Length::new(1.0), Mass::new(1.0),
-                              Position::new(0.0, 0.0), Velocity::new(0.0, 0.0));
-        let ball2 = Ball::new(Length::new(1.0), Mass::new(1.0),
-                              Position::new(1.0, 1.0), Velocity::new(1.0, 1.0));
+        let ball1 = SpyCircle::new(Position::new(0.0, 0.0), Length::new(1.0));
+        let ball2 = SpyCircle::new(Position::new(0.0, 0.0), Length::new(1.0));
 
         let bond = Bond::new(&ball1, &ball2);
 
         // TODO
     }
 
-    // TODO redundant
-    #[test]
-    fn new_bond_has_correct_ball_handles() {
-        let mut graph: SortableGraph<Ball, Bond> = SortableGraph::new();
+//    // TODO redundant
+//    #[test]
+//    fn new_bond_has_correct_ball_handles() {
+//        let mut graph: SortableGraph<Ball, Bond> = SortableGraph::new();
+//
+//        let h1 = graph.add_node(Ball::new(Length::new(1.0), Mass::new(1.0),
+//                                          Position::new(1.0, 1.0), Velocity::new(1.0, 1.0)));
+//        let h2 = graph.add_node(Ball::new(Length::new(1.0), Mass::new(1.0),
+//                                          Position::new(1.0, 1.0), Velocity::new(1.0, 1.0)));
+//
+//        let bond = Bond::new(graph.node(h1), graph.node(h2));
+//        graph.add_edge(bond);
+//
+//        let ball1 = &graph.nodes()[0];
+//        let ball2 = &graph.nodes()[1];
+//        assert_eq!(ball1, graph.node(bond.handle1()));
+//        assert_eq!(ball2, graph.node(bond.handle2()));
+//    }
 
-        let h1 = graph.add_node(Ball::new(Length::new(1.0), Mass::new(1.0),
-                                          Position::new(1.0, 1.0), Velocity::new(1.0, 1.0)));
-        let h2 = graph.add_node(Ball::new(Length::new(1.0), Mass::new(1.0),
-                                          Position::new(1.0, 1.0), Velocity::new(1.0, 1.0)));
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    pub struct SpyCircle {
+        handle: NodeHandle,
+        center: Position,
+        radius: Length,
+        pub strain: Displacement,
+    }
 
-        let bond = Bond::new(graph.node(h1), graph.node(h2));
-        graph.add_edge(bond);
+    impl SpyCircle {
+        pub fn new(center: Position, radius: Length) -> SpyCircle {
+            SpyCircle {
+                handle: NodeHandle::unset(),
+                center,
+                radius,
+                strain: Displacement::new(0.0, 0.0),
+            }
+        }
+    }
 
-        let ball1 = &graph.nodes()[0];
-        let ball2 = &graph.nodes()[1];
-        assert_eq!(ball1, graph.node(bond.handle1()));
-        assert_eq!(ball2, graph.node(bond.handle2()));
+    impl Circle for SpyCircle {
+        fn radius(&self) -> Length {
+            return self.radius;
+        }
+
+        fn center(&self) -> Position {
+            return self.center;
+        }
+    }
+
+    impl GraphNode for SpyCircle {
+        fn handle(&self) -> NodeHandle {
+            self.handle
+        }
+
+        fn handle_mut(&mut self) -> &mut NodeHandle {
+            &mut self.handle
+        }
     }
 }

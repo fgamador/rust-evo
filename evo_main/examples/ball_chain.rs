8b27053f3cec6473f2350ca93aad73e6fa664ac8
Franz Amador <franzamador@gmail.com>
2018-08-12 11:15:50 -0700

    add example main

---
+++ evo_main/examples/ball_chain.rs
@@ -0,0 +1,45 @@
+extern crate evo_main;
+extern crate evo_model;
+
+use evo_model::physics::ball::Ball;
+use evo_model::physics::bond::Bond;
+use evo_model::physics::quantities::*;
+use evo_model::world::World;
+use evo_main::main_support::init_and_run;
+
+fn main() {
+    init_and_run(create_world());
+}
+
+fn create_world() -> World {
+    let mut world = World::new(Position::new(-200.0, -200.0), Position::new(200.0, 200.0));
+
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, 100.0), Velocity::new(0.0, 0.0)));
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, 60.0), Velocity::new(0.0, 0.0)));
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, 20.0), Velocity::new(0.0, 0.0)));
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, -20.0), Velocity::new(0.0, 0.0)));
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, -60.0), Velocity::new(0.0, 0.0)));
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(0.0, -100.0), Velocity::new(0.0, 0.0)));
+
+    let bond = Bond::new(&world.balls()[0], &world.balls()[1]);
+    world.add_bond(bond);
+    let bond = Bond::new(&world.balls()[1], &world.balls()[2]);
+    world.add_bond(bond);
+    let bond = Bond::new(&world.balls()[2], &world.balls()[3]);
+    world.add_bond(bond);
+    let bond = Bond::new(&world.balls()[3], &world.balls()[4]);
+    world.add_bond(bond);
+    let bond = Bond::new(&world.balls()[4], &world.balls()[5]);
+    world.add_bond(bond);
+
+    world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
+                             Position::new(-40.0, 100.0), Velocity::new(-3.0, 0.0)));
+
+    world
+}

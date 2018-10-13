9864e1ccf2cb78d95317ca30bcc4d4e486cbd510
Franz Amador <franzamador@gmail.com>
2018-10-13 14:42:10 -0700

    r! switch to new method

--- evo_main/examples/bouncing_balls.rs
+++ evo_main/examples/bouncing_balls.rs
@@ -15,7 +15,7 @@ fn create_world() -> World<Ball> {
     let mut world =
         World::new(Position::new(-200.0, -200.0), Position::new(200.0, 200.0))
             .with_standard_walls()
-            .with_influences(vec![Box::new(PairCollisions::new())]);
+            .with_influence(Box::new(PairCollisions::new()));
     world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
                              Position::new(-100.0, -90.0), Velocity::new(3.0, 2.5)));
     world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),

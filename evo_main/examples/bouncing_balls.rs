723d69c084a1d5addadfe678301bc56c8024ed1c
Franz Amador <franzamador@gmail.com>
2018-10-13 14:35:21 -0700

    narrow set of influenced being used

--- evo_main/examples/bouncing_balls.rs
+++ evo_main/examples/bouncing_balls.rs
@@ -1,6 +1,7 @@
 extern crate evo_main;
 extern crate evo_model;
 
+use evo_model::environment::influences::*;
 use evo_model::physics::ball::Ball;
 use evo_model::physics::quantities::*;
 use evo_model::world::World;
@@ -11,7 +12,10 @@ fn main() {
 }
 
 fn create_world() -> World<Ball> {
-    let mut world = World::new2(Position::new(-200.0, -200.0), Position::new(200.0, 200.0));
+    let mut world =
+        World::new(Position::new(-200.0, -200.0), Position::new(200.0, 200.0))
+            .with_standard_walls()
+            .with_influences(vec![Box::new(PairCollisions::new())]);
     world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),
                              Position::new(-100.0, -90.0), Velocity::new(3.0, 2.5)));
     world.add_ball(Ball::new(Length::new(20.0), Mass::new(1.0),

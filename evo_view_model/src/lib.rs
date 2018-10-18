8f95347dfebc334f2d26683aaa5579d3afb1bf1e
Franz Amador <franzamador@gmail.com>
2018-10-17 20:43:19 -0700

    r remove commented-out code

--- evo_view_model/src/lib.rs
+++ evo_view_model/src/lib.rs
@@ -87,60 +87,6 @@ impl CoordinateTransform {
     }
 }
 
-//pub struct CoordinateTransform {
-//    input_window: Rectangle,
-//    output_window: Rectangle,
-//    scaling: f64,
-//}
-//
-//impl CoordinateTransform {
-//    pub fn new(input_window: Rectangle, output_window: Rectangle) -> Self {
-//        let mut transform = CoordinateTransform {
-//            input_window,
-//            output_window,
-//            scaling: 0.0,
-//        };
-//        transform.scaling = transform.calc_scale_x();
-//        if transform.scaling != transform.calc_scale_y() {
-//            panic!("Transform does not scale by the same factor in x ({}) and y ({})",
-//                   transform.scaling, transform.calc_scale_y());
-//        }
-//        transform
-//    }
-//
-//    fn calc_scale_x(&self) -> f64 {
-//        let input_width = self.input_window.max_corner().x() - self.input_window.min_corner().x();
-//        let output_width = self.output_window.max_corner().x() - self.output_window.min_corner().x();
-//        output_width / input_width
-//    }
-//
-//    fn calc_scale_y(&self) -> f64 {
-//        let input_height = self.input_window.max_corner().y() - self.input_window.min_corner().y();
-//        let output_height = self.output_window.max_corner().y() - self.output_window.min_corner().y();
-//        output_height / input_height
-//    }
-//
-//    pub fn transform_position(&self, input_position: Position) -> Position {
-//        let x = self.transform_x(input_position.x());
-//        let y = self.transform_y(input_position.y());
-//        Position::new(x, y)
-//    }
-//
-//    fn transform_x(&self, input_x: f64) -> f64 {
-//        let input_delta_x = input_x - self.input_window.min_corner().x();
-//        self.output_window.min_corner().x() + self.scaling * input_delta_x
-//    }
-//
-//    fn transform_y(&self, input_y: f64) -> f64 {
-//        let input_delta_y = input_y - self.input_window.min_corner().y();
-//        self.output_window.min_corner().y() + self.scaling * input_delta_y
-//    }
-//
-//    pub fn transform_length(&self, len: Length) -> Length {
-//        len * self.scaling
-//    }
-//}
-
 #[cfg(test)]
 mod tests {
     use super::*;

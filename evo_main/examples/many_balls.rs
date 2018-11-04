0bda34840429cd60c8673c9105e147a6ddbeeada
Franz Amador <franzamador@gmail.com>
2018-11-03 21:59:13 -0700

    more balls

--- evo_main/examples/many_balls.rs
+++ evo_main/examples/many_balls.rs
@@ -15,12 +15,12 @@ fn create_world() -> World<Ball> {
     let mut world = World::new(Position::new(-750.0, -350.0), Position::new(750.0, 350.0))
         .with_perimeter_walls()
         .with_influence(Box::new(PairCollisions::new()));
-    for i in 0..36 {
-        for j in 0..16 {
+    for i in 0..48 {
+        for j in 0..21 {
             world = world.with_ball(Ball::new(Length::new(10.0), Mass::new(1.0),
-                                              Position::new(-700.0 + (i * 40) as f64,
-                                                            -300.0 + (j * 40) as f64),
-                                              Velocity::new(1.0, 1.0)));
+                                              Position::new(-700.0 + (i * 30) as f64,
+                                                            -300.0 + (j * 30) as f64),
+                                              Velocity::new(2.0, 2.0)));
         }
     }
     world

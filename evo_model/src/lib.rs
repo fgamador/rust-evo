076624bff3777ae3bf9ce93a6835c95d43f9f2d0
Franz Amador <franzamador@gmail.com>
2018-11-04 11:06:52 -0800

    t tick_populates_view_model_circle_from_ball

--- evo_model/src/lib.rs
+++ evo_model/src/lib.rs
@@ -43,3 +43,40 @@ fn to_circle<T>(ball: &T) -> evo_view_model::Circle
         radius: ball.radius().value(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use physics::ball::Ball;
+    use physics::quantities::*;
+
+    #[test]
+    fn tick_creates_view_model_circle_for_each_ball() {
+        let mut world = World::new(Position::new(0.0, 0.0), Position::new(0.0, 0.0));
+        world.add_ball(Ball::new(Length::new(1.0), Mass::new(1.0),
+                                 Position::new(0.0, 0.0), Velocity::new(0.0, 0.0)));
+        world.add_ball(Ball::new(Length::new(1.0), Mass::new(1.0),
+                                 Position::new(0.0, 0.0), Velocity::new(0.0, 0.0)));
+        let mut view_model = ViewModel::new();
+
+        tick(&mut world, &mut view_model);
+
+        assert_eq!(2, view_model.circles.len());
+    }
+
+    #[test]
+    fn tick_populates_view_model_circle_from_ball() {
+        let mut world = World::new(Position::new(0.0, 0.0), Position::new(0.0, 0.0));
+        world.add_ball(Ball::new(Length::new(5.0), Mass::new(1.0),
+                                 Position::new(2.0, -3.0), Velocity::new(0.0, 0.0)));
+        let mut view_model = ViewModel::new();
+
+        tick(&mut world, &mut view_model);
+
+        let ball = &world.balls()[0];
+        let circle = view_model.circles[0];
+        assert_eq!(circle.center.x, ball.center().x());
+        assert_eq!(circle.center.y, ball.center().y());
+        assert_eq!(circle.radius, ball.radius().value());
+    }
+}

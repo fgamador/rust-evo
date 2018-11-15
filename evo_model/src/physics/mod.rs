80b00b83ba7372d877f06c54fea5a505b6571a1e
Franz Amador <franzamador@gmail.com>
2018-11-14 21:00:23 -0800

    Add class

--- evo_model/src/physics/mod.rs
+++ evo_model/src/physics/mod.rs
@@ -1,9 +1,10 @@
 pub mod ball;
-pub mod sortable_graph;
 pub mod bond;
 pub mod newtonian;
 pub mod overlap;
 pub mod quantities;
 pub mod shapes;
 pub mod simple_graph_elements;
+pub mod sortable_graph;
+pub mod spring;
 pub mod util;

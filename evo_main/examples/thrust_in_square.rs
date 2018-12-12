2ff667d696f86459da09b2345ff756786708d2c9
Franz Amador <franzamador@gmail.com>
2018-12-11 21:47:46 -0800

    new demo (weird)

---
+++ evo_main/examples/thrust_in_square.rs
@@ -0,0 +1,33 @@
+extern crate evo_main;
+extern crate evo_model;
+extern crate evo_view_model;
+
+use evo_model::biology::control::*;
+use evo_model::biology::layers::*;
+use evo_model::biology::cell::Cell;
+use evo_model::environment::influences::*;
+use evo_model::physics::quantities::*;
+use evo_model::world::World;
+use evo_main::main_support::init_and_run;
+use std::f64::consts::PI;
+
+fn main() {
+    init_and_run(create_world());
+}
+
+fn create_world() -> World<Cell> {
+    World::new(Position::new(0.0, -400.0), Position::new(400.0, 0.0))
+        .with_perimeter_walls()
+        .with_influences(vec![
+            Box::new(SimpleForceInfluence::new(Box::new(DragForce::new(0.001))))
+        ])
+        .with_cells(vec![
+            Cell::new(
+                Position::new(300.0, -300.0), Velocity::new(0.0, 0.0),
+                vec![
+                    Box::new(ThrusterLayer::new(Area::new(200.0 * PI))),
+                ])
+                .with_control(Box::new(
+                    ThrustInSquareControl::new(0, 10.0, Direction::Left, 100))),
+        ])
+}

9f4dfc166108763fda968d32c4bc4e181bb090da
Franz Amador <franzamador@gmail.com>
2018-12-15 10:49:37 -0800

    add method (and tests)

--- evo_model/src/environment/environment.rs
+++ evo_model/src/environment/environment.rs
@@ -9,11 +9,15 @@ pub trait HasLocalEnvironment {
 #[derive(Clone, Debug)]
 pub struct LocalEnvironment {
     overlaps: Vec<Overlap>,
+    light_intensity: f64,
 }
 
 impl LocalEnvironment {
     pub fn new() -> Self {
-        LocalEnvironment { overlaps: vec![] }
+        LocalEnvironment {
+            overlaps: vec![],
+            light_intensity: 0.0,
+        }
     }
 
     pub fn add_overlap(&mut self, overlap: Overlap) {
@@ -24,8 +28,15 @@ impl LocalEnvironment {
         &self.overlaps
     }
 
+    pub fn add_light_intensity(&mut self, light_intensity: f64) {
+        self.light_intensity += light_intensity;
+    }
+
+    pub fn light_intensity(&self) -> f64 { self.light_intensity }
+
     pub fn clear(&mut self) {
         self.overlaps.clear();
+        self.light_intensity = 0.0;
     }
 }
 
@@ -35,10 +46,30 @@ mod tests {
     use physics::quantities::*;
 
     #[test]
+    fn add_overlap() {
+        let mut env = LocalEnvironment::new();
+        env.add_overlap(Overlap::new(Displacement::new(1.0, 1.0)));
+        env.add_overlap(Overlap::new(Displacement::new(1.0, 1.0)));
+        assert_eq!(2, env.overlaps().len());
+    }
+
+    #[test]
+    fn add_light_intensity() {
+        let mut env = LocalEnvironment::new();
+        env.add_light_intensity(1.0);
+        env.add_light_intensity(1.0);
+        assert_eq!(2.0, env.light_intensity());
+    }
+
+    #[test]
     fn clear_local_environment() {
         let mut env = LocalEnvironment::new();
         env.add_overlap(Overlap::new(Displacement::new(1.0, 1.0)));
+        env.add_light_intensity(1.0);
+
         env.clear();
+
         assert!(env.overlaps().is_empty());
+        assert_eq!(0.0, env.light_intensity());
     }
 }

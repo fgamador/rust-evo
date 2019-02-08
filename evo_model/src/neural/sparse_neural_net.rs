25a91b9e2c2a653300803b3a42711c89d6cf073f
Franz Amador <franzamador@gmail.com>
2019-02-07 21:34:23 -0800

    t initial_fully_connected

--- evo_model/src/neural/sparse_neural_net.rs
+++ evo_model/src/neural/sparse_neural_net.rs
@@ -4,17 +4,11 @@
 
 use std::f32;
 
-struct Op {
+pub struct Op {
     input_index: u16,
     output_index: u16,
     weight: f32,
-    op_fn: fn(op: &Op, node_values: &mut Vec<f32>),
-}
-
-impl Op {
-    fn add_weighted(op: &Op, node_values: &mut Vec<f32>) {
-        node_values[op.output_index as usize] += op.weight * node_values[op.input_index as usize];
-    }
+    op_fn: fn(&Op, &mut Vec<f32>),
 }
 
 pub struct NeuralNet {
@@ -22,28 +16,22 @@ pub struct NeuralNet {
     num_outputs: u16,
     node_values: Vec<f32>,
     ops: Vec<Op>,
-    initial_weight: f32,
-    activation_fn: fn(f32) -> f32,
 }
 
 impl NeuralNet {
-    pub fn new(num_inputs: u16, num_outputs: u16, initial_weight: f32, activation_fn: fn(f32) -> f32) -> Self {
+    pub fn new(num_inputs: u16, num_outputs: u16, initial_weight: f32, transfer_op: fn(&Op, &mut Vec<f32>)) -> Self {
         let mut nnet = NeuralNet {
             num_inputs,
             num_outputs,
             node_values: vec![],
             ops: Vec::with_capacity((num_inputs * num_outputs + num_outputs) as usize),
-            initial_weight,
-            activation_fn,
         };
         nnet.node_values.resize((num_inputs + num_outputs) as usize, 0.0);
-        for output_index in 0..num_outputs {
+        for output_index in num_inputs..num_inputs + num_outputs {
             for input_index in 0..num_inputs {
-                nnet.ops.push(Op { input_index, output_index, weight: initial_weight, op_fn: Op::add_weighted });
+                nnet.ops.push(Op { input_index, output_index, weight: initial_weight, op_fn: Self::add_weighted });
             }
-        }
-        for output_index in 0..num_outputs {
-            nnet.ops.push(Op { input_index: 0, output_index, weight: 0.0, op_fn: Op::add_weighted });
+            nnet.ops.push(Op { input_index: 0, output_index, weight: 0.0, op_fn: transfer_op });
         }
         nnet
     }
@@ -54,8 +42,11 @@ impl NeuralNet {
     }
 
     pub fn run(&mut self) {
+        for op in &self.ops {
+            (op.op_fn)(op, &mut self.node_values);
+        }
         // TODO initial hack
-        self.node_values[1] = (self.activation_fn)(self.initial_weight * self.node_values[0]);
+        //self.node_values[1] = (self.activation_fn)(self.initial_weight * self.node_values[0]);
     }
 
     pub fn output(&self, index: usize) -> f32 {
@@ -63,16 +54,19 @@ impl NeuralNet {
         self.node_values[self.num_inputs as usize + index]
     }
 
-    pub fn identity(val: f32) -> f32 {
-        val
+    pub fn identity(_op: &Op, _node_values: &mut Vec<f32>) {}
+
+    pub fn plus_one(op: &Op, node_values: &mut Vec<f32>) {
+        node_values[op.output_index as usize] += 1.0;
     }
 
-    pub fn plus_one(val: f32) -> f32 {
-        val + 1.0
-    }
-
+    // TODO turn into op
     pub fn sigmoidal(val: f32) -> f32 {
         1.0_f32 / (1.0_f32 + (-4.9_f32 * val).exp())
+    }
+
+    fn add_weighted(op: &Op, node_values: &mut Vec<f32>) {
+        node_values[op.output_index as usize] += op.weight * node_values[op.input_index as usize];
     }
 }
 
@@ -81,14 +75,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simplest_net() {
-        let mut nnet = NeuralNet::new(1, 1, 0.5, NeuralNet::plus_one);
-        nnet.set_input(0, 3.0);
-        nnet.run();
-        assert_eq!(nnet.output(0), 2.5);
-    }
-
-    //#[test]
     fn initial_fully_connected() {
         let mut nnet = NeuralNet::new(3, 2, 0.5, NeuralNet::plus_one);
         nnet.set_input(0, 2.0);

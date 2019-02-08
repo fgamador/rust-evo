e5a45faf3638fb9d67b8452578907af68f58d29e
Franz Amador <franzamador@gmail.com>
2019-02-07 21:02:41 -0800

    Failing test with solution sketch

--- evo_model/src/neural/sparse_neural_net.rs
+++ evo_model/src/neural/sparse_neural_net.rs
@@ -4,10 +4,24 @@
 
 use std::f32;
 
+struct Op {
+    input_index: u16,
+    output_index: u16,
+    weight: f32,
+    op_fn: fn(op: &Op, node_values: &mut Vec<f32>),
+}
+
+impl Op {
+    fn add_weighted(op: &Op, node_values: &mut Vec<f32>) {
+        node_values[op.output_index as usize] += op.weight * node_values[op.input_index as usize];
+    }
+}
+
 pub struct NeuralNet {
     num_inputs: u16,
     num_outputs: u16,
     node_values: Vec<f32>,
+    ops: Vec<Op>,
     initial_weight: f32,
     activation_fn: fn(f32) -> f32,
 }
@@ -18,10 +32,19 @@ impl NeuralNet {
             num_inputs,
             num_outputs,
             node_values: vec![],
+            ops: Vec::with_capacity((num_inputs * num_outputs + num_outputs) as usize),
             initial_weight,
             activation_fn,
         };
         nnet.node_values.resize((num_inputs + num_outputs) as usize, 0.0);
+        for output_index in 0..num_outputs {
+            for input_index in 0..num_inputs {
+                nnet.ops.push(Op { input_index, output_index, weight: initial_weight, op_fn: Op::add_weighted });
+            }
+        }
+        for output_index in 0..num_outputs {
+            nnet.ops.push(Op { input_index: 0, output_index, weight: 0.0, op_fn: Op::add_weighted });
+        }
         nnet
     }
 
@@ -65,14 +88,16 @@ mod tests {
         assert_eq!(nnet.output(0), 2.5);
     }
 
-//    #[test]
-//    fn initial_fully_connected() {
-//        let mut nnet = NeuralNet::new(3, 2, 0.5);
-//        nnet.set_input(0, 2.0);
-//        nnet.set_input(1, 3.0);
-//        nnet.set_input(2, 4.0);
-//        nnet.run();
-//        assert_eq!(nnet.output(0), 2.0);
-//        assert_eq!(nnet.output(1), 2.0);
-//    }
+    //#[test]
+    fn initial_fully_connected() {
+        let mut nnet = NeuralNet::new(3, 2, 0.5, NeuralNet::plus_one);
+        nnet.set_input(0, 2.0);
+        nnet.set_input(1, 3.0);
+        nnet.set_input(2, 4.0);
+        nnet.run();
+        assert_eq!(nnet.output(0), 5.5);
+        assert_eq!(nnet.output(1), 5.5);
+    }
+
+    // TODO clear between runs
 }

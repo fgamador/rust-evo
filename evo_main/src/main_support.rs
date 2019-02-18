c73fbcdec442a8bde150b9d8a12ec40fe5c10a8d
Franz Amador <franzamador@gmail.com>
2019-02-18 12:54:40 -0800

    r remove dead code

--- evo_main/src/main_support.rs
+++ evo_main/src/main_support.rs
@@ -1,34 +1,14 @@
 use evo_model::world::World;
-use evo_view_model::Event;
 use evo_view_model::ViewModel;
-use evo_view_model::events::EventManager;
 use mvvm::*;
 
 pub fn init_and_run(world: World) {
-    let mut event_manager: EventManager<Event, MVVM> = EventManager::new();
-//    wire_up_events(&mut event_manager);
     let view = View::new(world.min_corner(), world.max_corner());
     let mvvm = MVVM(Model::new(world), view, ViewModel::new());
-    run(event_manager, mvvm);
+    run(mvvm);
 }
 
-fn _wire_up_events(event_manager: &mut EventManager<Event, MVVM>) {
-    event_manager.add_listener(Event::Rendered, |event_queue, subject| {
-        let MVVM(ref mut model, _, ref mut view_model) = subject;
-        model.tick(view_model);
-        event_queue.push(Event::Updated);
-    });
-    event_manager.add_listener(Event::Updated, |event_queue, subject| {
-        let MVVM(_, ref mut view, ref mut view_model) = subject;
-        if view.render(view_model) {
-            event_queue.push(Event::Rendered);
-        }
-    });
-}
-
-fn run(mut _event_manager: EventManager<Event, MVVM>, mut mvvm: MVVM) {
-//    event_manager.events().push(Event::Rendered);
-//    event_manager.fire_events(&mut mvvm);
+fn run(mut mvvm: MVVM) {
     let mut done = false;
     while !done {
         let MVVM(ref mut model, ref mut view, ref mut view_model) = mvvm;

7cbfd993c158e2b4b539b91af68301957282472b
Franz Amador <franzamador@gmail.com>
2019-02-19 20:56:15 -0800

    'flat' interpolation qualifier fixes the problem!

--- evo_glium/src/bullseye_drawing.rs
+++ evo_glium/src/bullseye_drawing.rs
@@ -3,11 +3,11 @@ use glium::Surface;
 #[derive(Clone, Copy)]
 pub struct Bullseye {
     pub center: [f32; 2],
-    pub radii_0_3: [f32; 4],
     pub num_radii: u32,
+    pub radii_0_3: [f32; 4],
 }
 
-implement_vertex!(Bullseye, center, radii_0_3, num_radii);
+implement_vertex!(Bullseye, center, num_radii, radii_0_3);
 
 pub struct BullseyeDrawing {
     pub shader_program: glium::Program,
@@ -36,20 +36,19 @@ impl BullseyeDrawing {
         #version 330 core
 
         in vec2 center;
-        in vec4 radii_0_3;
         in uint num_radii;
+        in vec4 radii_0_3;
 
         out Circle {
             vec2 center;
-            float radii[4];
             uint num_radii;
+            vec4 radii;
         } circle_out;
 
         void main() {
             circle_out.center = center;
-            circle_out.radii = float[](radii_0_3[0], radii_0_3[1], radii_0_3[2], radii_0_3[3]);
-            //circle_out.num_radii = num_radii;
-            circle_out.num_radii = 1u;
+            circle_out.num_radii = num_radii;
+            circle_out.radii = radii_0_3;
         }
     "#;
 
@@ -63,19 +62,21 @@ impl BullseyeDrawing {
 
         in Circle {
             vec2 center;
-            float radii[4];
             uint num_radii;
+            vec4 radii;
         } circle_in[];
 
         out CirclePoint {
             vec2 offset;
-            float radii[4];
-            uint num_radii;
+            flat uint num_radii;
+            flat vec4 radii;
         } circle_point_out;
 
         void emit_circle_bounding_box_corner(in vec2 center, in float radius, in vec2 corner) {
             vec2 offset = vec2(radius, radius) * corner;
             circle_point_out.offset = offset;
+            circle_point_out.num_radii = circle_in[0].num_radii;
+            circle_point_out.radii = circle_in[0].radii;
             gl_Position = screen_transform * vec4(center + offset, 0.0, 1.0);
             EmitVertex();
         }
@@ -89,10 +90,7 @@ impl BullseyeDrawing {
         }
 
         void main() {
-            //uint num_radii = circle_in[0].num_radii;
-            uint num_radii = 1u;
-            circle_point_out.radii = circle_in[0].radii;
-            circle_point_out.num_radii = num_radii;
+            uint num_radii = circle_in[0].num_radii;
             float radius = circle_in[0].radii[num_radii - 1u];
             emit_circle_bounding_box(circle_in[0].center, radius);
         }
@@ -105,8 +103,8 @@ impl BullseyeDrawing {
 
         in CirclePoint {
             vec2 offset;
-            float radii[4];
-            uint num_radii;
+            flat uint num_radii;
+            flat vec4 radii;
         } circle_point_in;
 
         out vec4 color;
@@ -114,9 +112,7 @@ impl BullseyeDrawing {
         void main() {
             float radius = circle_point_in.radii[circle_point_in.num_radii - 1u];
             float dist = sqrt(dot(circle_point_in.offset, circle_point_in.offset));
-            if (dist <= radius / 2.0)
-                color = circle_color / 2.0;
-            else if (dist <= radius)
+            if (dist <= radius)
                 color = circle_color;
             else
                 discard;

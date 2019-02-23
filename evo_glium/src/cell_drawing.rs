6bd50fec7884c0323939665719de688bd0a0c5bd
Franz Amador <franzamador@gmail.com>
2019-02-23 15:16:26 -0800

    adjust saturation per health

--- evo_glium/src/cell_drawing.rs
+++ evo_glium/src/cell_drawing.rs
@@ -128,7 +128,7 @@ impl CellDrawing {
         out vec4 color_out;
 
         // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
-        vec3 rgb2hsv(vec3 c) {
+        vec3 rgb2hsv(in vec3 c) {
             vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
             vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
             vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
@@ -139,16 +139,24 @@ impl CellDrawing {
         }
 
         // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
-        vec3 hsv2rgb(vec3 c) {
+        vec3 hsv2rgb(in vec3 c) {
             vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
             vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
             return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
         }
 
+        vec4 adjust_color_per_health(in vec4 color, in float health) {
+            vec3 hsv = rgb2hsv(color.rgb);
+            float adjusted_s = health * hsv[1];
+            float adjusted_v = (0.5 + 0.5 * health) * hsv[2];
+            return vec4(hsv2rgb(vec3(hsv[0], adjusted_s, adjusted_v)), color.a);
+        }
+
         void emit_color(in uint layer_index, in float health) {
-            color_out = (layer_index < 4u)
+            vec4 pure_color = (layer_index < 4u)
                 ? layer_colors_0_3[layer_index]
                 : layer_colors_4_7[layer_index - 4u];
+            color_out = adjust_color_per_health(pure_color, health);
         }
 
         void main() {

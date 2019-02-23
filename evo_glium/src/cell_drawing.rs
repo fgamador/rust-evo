87b626ae7a41fe100894ebbd5415933bd40c36ac
Franz Amador <franzamador@gmail.com>
2019-02-23 15:05:31 -0800

    prepare for adjusting saturation per health

--- evo_glium/src/cell_drawing.rs
+++ evo_glium/src/cell_drawing.rs
@@ -127,7 +127,25 @@ impl CellDrawing {
 
         out vec4 color_out;
 
-        void emit_color(in uint layer_index) {
+        // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+        vec3 rgb2hsv(vec3 c) {
+            vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+            vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+            vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+            float d = q.x - min(q.w, q.y);
+            float e = 1.0e-10;
+            return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+        }
+
+        // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+        vec3 hsv2rgb(vec3 c) {
+            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+            return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+        }
+
+        void emit_color(in uint layer_index, in float health) {
             color_out = (layer_index < 4u)
                 ? layer_colors_0_3[layer_index]
                 : layer_colors_4_7[layer_index - 4u];
@@ -137,7 +155,7 @@ impl CellDrawing {
             float dist = sqrt(dot(cell_point_in.offset, cell_point_in.offset));
             for (uint i = 0u; i < min(8u, cell_point_in.num_radii); ++i) {
                 if (dist <= cell_point_in.radii[i]) {
-                    emit_color(i);
+                    emit_color(i, cell_point_in.health[i]);
                     return;
                 }
             }

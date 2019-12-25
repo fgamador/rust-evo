7ef57779f40d0c1fc52e572ddc7bbd89dda95083
Franz Amador <franzamador@gmail.com>
2019-12-25 12:07:27 -0800

    use defaults to hide unimportant test constants

--- evo_main/examples/decay_healing_and_death.rs
+++ evo_main/examples/decay_healing_and_death.rs
@@ -31,7 +31,7 @@ fn create_world() -> World {
                     .with_health_parameters(LayerHealthParameters {
                         healing_energy_delta: BioEnergyDelta::ZERO,
                         entropic_damage_health_delta: -0.006,
-                        overlap_damage_health_delta: 0.0,
+                        ..LayerHealthParameters::DEFAULT
                     })),
             ])
             .with_control(Box::new(GrowThenHealControl::new(0, 100, AreaDelta::new(10.0), 100, 0.01))))

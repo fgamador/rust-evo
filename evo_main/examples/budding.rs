1d7c48a9625e80f9c0fe8158cc854dd2ad772c9b
Franz Amador <franzamador@gmail.com>
2020-01-05 12:45:46 -0800

    r! extract var

--- evo_main/examples/budding.rs
+++ evo_main/examples/budding.rs
@@ -74,11 +74,15 @@ impl BuddingControl {
             donation_energy = BioEnergy::new(1.0);
             self.budding_angle += Deflection::from_radians(PI / 4.0);
         }
-        vec![
-            EnergyGeneratingCellLayerSpecialty::energy_request(0, BioEnergy::new(1.0)),
+        let mut budding_requests = vec![
             BuddingCellLayerSpecialty::budding_angle_request(self.budding_layer_index, self.budding_angle),
             BuddingCellLayerSpecialty::donation_energy_request(self.budding_layer_index, donation_energy),
-        ]
+        ];
+        let mut requests = vec![
+            EnergyGeneratingCellLayerSpecialty::energy_request(0, BioEnergy::new(1.0)),
+        ];
+        requests.append(&mut budding_requests);
+        requests
     }
 
     fn youth_requests(budding_layer_index: usize) -> Vec<ControlRequest> {

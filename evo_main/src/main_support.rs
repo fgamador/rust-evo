e3db92c1677f5004bacfe2e2180bf612fa468ef1
Franz Amador <franzamador@gmail.com>
2020-01-06 22:38:05 -0800

    return user action that stops play

--- evo_main/src/main_support.rs
+++ evo_main/src/main_support.rs
@@ -13,14 +13,17 @@ fn run(mut world: World, mut view: View) {
     normal_speed(&mut world, &mut view);
 }
 
-fn normal_speed(world: &mut World, view: &mut View) {
+fn normal_speed(world: &mut World, view: &mut View) -> UserAction {
     let mut next_tick = Instant::now();
-    while view.check_for_user_action() != Some(UserAction::Exit) {
+    let mut user_action= view.check_for_user_action();
+    while user_action == None {
         view.render(world);
         next_tick += Duration::from_millis(16);
         await_next_tick(next_tick);
         world.tick();
+        user_action= view.check_for_user_action();
     }
+    user_action.unwrap()
 }
 
 fn await_next_tick(next_tick: Instant) {

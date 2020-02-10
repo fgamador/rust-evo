021ee8cf90b517a90070b59d7dafec453bf0fb15
Franz Amador <franzamador@gmail.com>
2020-02-09 16:18:17 -0800

    neural net controls growth

--- evo_main/examples/neural_net_budding.rs
+++ evo_main/examples/neural_net_budding.rs
@@ -6,10 +6,13 @@ use evo_model::biology::cell::Cell;
 use evo_model::biology::control::*;
 use evo_model::biology::control_requests::*;
 use evo_model::biology::layers::*;
+use evo_model::environment::influences::*;
 use evo_model::genome::sparse_neural_net::*;
 use evo_model::physics::quantities::*;
 use evo_model::world::World;
 use std::f64::consts::PI;
+
+type VecIndex = u16;
 
 fn main() {
     init_and_run(create_world());
@@ -19,6 +22,10 @@ fn create_world() -> World {
     World::new(Position::new(0.0, -400.0), Position::new(400.0, 0.0))
         .with_perimeter_walls()
         .with_pair_collisions()
+        .with_sunlight(0.0, 1.0)
+        .with_influences(vec![
+            Box::new(SimpleForceInfluence::new(Box::new(DragForce::new(0.005)))),
+        ])
         .with_cell(
             create_child(0, &MutationParameters::NO_MUTATION)
                 .with_initial_position(Position::new(200.0, -100.0)),
@@ -34,8 +41,14 @@ fn create_child(seed: u64, mutation_parameters: &'static MutationParameters) -> 
                 Area::new(5.0 * PI),
                 Density::new(1.0),
                 Color::Green,
-                Box::new(EnergyGeneratingCellLayerSpecialty::new()),
-            ),
+                Box::new(PhotoCellLayerSpecialty::new(0.1)),
+            )
+            .with_resize_parameters(LayerResizeParameters {
+                growth_energy_delta: BioEnergyDelta::new(-1.0),
+                max_growth_rate: 10.0,
+                shrinkage_energy_delta: BioEnergyDelta::new(0.0),
+                max_shrinkage_rate: 0.1,
+            }),
             CellLayer::new(
                 Area::new(5.0 * PI),
                 Density::new(1.0),
@@ -45,7 +58,13 @@ fn create_child(seed: u64, mutation_parameters: &'static MutationParameters) -> 
                     mutation_parameters,
                     create_child,
                 )),
-            ),
+            )
+            .with_resize_parameters(LayerResizeParameters {
+                growth_energy_delta: BioEnergyDelta::new(-1.0),
+                max_growth_rate: 10.0,
+                shrinkage_energy_delta: BioEnergyDelta::new(0.0),
+                max_shrinkage_rate: 0.1,
+            }),
         ],
     )
     .with_control(Box::new(NeuralNetBuddingControl::new()))
@@ -59,10 +78,23 @@ pub struct NeuralNetBuddingControl {
 }
 
 impl NeuralNetBuddingControl {
+    const PHOTO_LAYER_AREA_INPUT_INDEX: VecIndex = 0;
+    const CELL_ENERGY_INPUT_INDEX: VecIndex = 1;
+    const PHOTO_LAYER_RESIZE_OUTPUT_INDEX: VecIndex = 2;
+    const DONATION_ENERGY_OUTPUT_INDEX: VecIndex = 3;
+
     fn new() -> Self {
         let mut nnet = SparseNeuralNet::new(TransferFn::IDENTITY);
-        nnet.connect_node(1, -100.0, vec![(0, -1.0)]);
-        nnet.connect_node(3, -100.0, vec![(0, -1.0)]);
+        nnet.connect_node(
+            Self::PHOTO_LAYER_RESIZE_OUTPUT_INDEX,
+            800.0,
+            vec![(Self::PHOTO_LAYER_AREA_INPUT_INDEX, -1.0)],
+        );
+        nnet.connect_node(
+            Self::DONATION_ENERGY_OUTPUT_INDEX,
+            -100.0,
+            vec![(Self::CELL_ENERGY_INPUT_INDEX, -1.0)],
+        );
         NeuralNetBuddingControl {
             nnet,
             budding_ticks: 100,
@@ -106,28 +138,33 @@ impl NeuralNetBuddingControl {
 
 impl CellControl for NeuralNetBuddingControl {
     fn get_control_requests(&mut self, cell_state: &CellStateSnapshot) -> Vec<ControlRequest> {
-        let radius_input = cell_state.radius.value() as f32;
+        let energy_layer_area_input = cell_state.layers[0].area.value() as f32;
         let energy_input = cell_state.energy.value() as f32;
-        self.nnet.set_node_value(0, radius_input);
-        self.nnet.set_node_value(1, energy_input);
+        self.nnet
+            .set_node_value(Self::PHOTO_LAYER_AREA_INPUT_INDEX, energy_layer_area_input);
+        self.nnet
+            .set_node_value(Self::CELL_ENERGY_INPUT_INDEX, energy_input);
 
-        //        self.nnet.run();
+        self.nnet.run();
 
-        let layers_resize_output = self.nnet.node_value(2) as f64;
-        let donation_energy_resize_output = self.nnet.node_value(3) as f64;
+        let energy_layer_resize_output =
+            self.nnet.node_value(Self::PHOTO_LAYER_RESIZE_OUTPUT_INDEX) as f64;
+        let donation_energy_output =
+            self.nnet.node_value(Self::DONATION_ENERGY_OUTPUT_INDEX) as f64;
         let _nnet_requests = vec![
-            CellLayer::resize_request(0, AreaDelta::new(layers_resize_output)),
-            CellLayer::resize_request(1, AreaDelta::new(layers_resize_output)),
-            BuddingCellLayerSpecialty::donation_energy_request(
-                1,
-                BioEnergy::new(donation_energy_resize_output),
-            ),
+            CellLayer::resize_request(0, AreaDelta::new(energy_layer_resize_output)),
+            CellLayer::resize_request(1, AreaDelta::new(energy_layer_resize_output / 2.0)),
+            //            BuddingCellLayerSpecialty::donation_energy_request(
+            //                1,
+            //                BioEnergy::new(donation_energy_output),
+            //            ),
         ];
+        return _nnet_requests;
 
-        if Self::is_adult(cell_state) {
-            self.adult_requests()
-        } else {
-            self.youth_requests()
-        }
+        //        if Self::is_adult(cell_state) {
+        //            self.adult_requests()
+        //        } else {
+        //            self.youth_requests()
+        //        }
     }
 }

55751d207caae0b2118ee1c06b27e3bcb6487dd8
Franz Amador <franzamador@gmail.com>
2020-02-29 11:04:37 -0800

    r! extract fn

--- evo_model/src/physics/sortable_graph.rs
+++ evo_model/src/physics/sortable_graph.rs
@@ -41,16 +41,18 @@ impl<N: GraphNode, E: GraphEdge, ME: GraphMetaEdge> SortableGraph<N, E, ME> {
     /// Never retain handles across a call to this function.
     pub fn remove_nodes(&mut self, handles: &[NodeHandle]) {
         for handle in handles.iter().rev() {
-            self.remove_edges(&self.node(*handle).graph_node_data().edge_handles.clone());
-            self.unsorted_nodes.swap_remove(handle.index);
-            if handle.index < self.unsorted_nodes.len() {
-                self.fix_swapped_node_and_its_edges(
-                    NodeHandle::new(self.unsorted_nodes.len()),
-                    *handle,
-                );
-            }
+            self.remove_node(*handle);
         }
         self.remove_obsolete_node_handles();
+    }
+
+    /// Warning: invalidates handles to the last node in unsorted_nodes.
+    fn remove_node(&mut self, handle: NodeHandle) {
+        self.remove_edges(&self.node(handle).graph_node_data().edge_handles.clone());
+        self.unsorted_nodes.swap_remove(handle.index);
+        if handle.index < self.unsorted_nodes.len() {
+            self.fix_swapped_node_and_its_edges(NodeHandle::new(self.unsorted_nodes.len()), handle);
+        }
     }
 
     fn fix_swapped_node_and_its_edges(&mut self, old_handle: NodeHandle, new_handle: NodeHandle) {

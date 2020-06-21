414328ad19c3f0f102b9d196e877d7436c16c70c
Franz Amador <franzamador@gmail.com>
2020-06-20 18:09:13 -0700

    t pair_no_collision_force

--- evo_model/src/environment/influences.rs
+++ evo_model/src/environment/influences.rs
@@ -537,6 +537,20 @@ mod tests {
     }
 
     #[test]
+    fn pair_no_collision_force() {
+        assert_eq!(
+            PairCollisions::collision_force(
+                Mass::new(2.0),
+                Velocity::new(3.0, -4.0),
+                Displacement::new(0.0, 0.0),
+                Mass::new(6.0),
+                Velocity::new(-5.0, 6.0),
+            ),
+            Force::new(0.0, 0.0)
+        );
+    }
+
+    #[test]
     fn pair_fast_collision_force() {
         assert_eq!(
             PairCollisions::collision_force(

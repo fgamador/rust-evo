2f954150fc4ea03a73001696f2135d83b6f543ba
Franz Amador <franzamador@gmail.com>
2020-06-29 19:01:13 -0700

    improve demo

--- evo_main/examples/bonded_pair.rs
+++ evo_main/examples/bonded_pair.rs
@@ -17,15 +17,78 @@ fn create_world() -> World {
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(-30.0, 0.0),
-                Velocity::new(-5.0, 2.0),
+                Position::new(-25.0, 140.0),
+                Velocity::new(0.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(25.0, 0.0),
-                Velocity::new(0.0, -1.0),
+                Position::new(25.0, 140.0),
+                Velocity::new(0.0, 0.0),
             ),
         ])
         .with_bonds(vec![(0, 1)])
+        .with_cells(vec![
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(-160.0, 50.0),
+                Velocity::new(4.0, 0.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(0.0, 50.0),
+                Velocity::new(0.0, 0.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(40.0, 50.0),
+                Velocity::new(0.0, 0.0),
+            ),
+        ])
+        .with_bonds(vec![(3, 4)])
+        .with_cells(vec![
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(-160.0, -50.0),
+                Velocity::new(4.0, 0.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(0.0, -50.0),
+                Velocity::new(0.0, 0.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(40.0, -50.0),
+                Velocity::new(0.0, 0.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(80.0, -50.0),
+                Velocity::new(0.0, 0.0),
+            ),
+        ])
+        .with_bonds(vec![(6, 7)])
+        .with_cells(vec![
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(-20.0, -140.0),
+                Velocity::new(0.0, 1.0),
+            ),
+            Cell::ball(
+                Length::new(20.0),
+                Mass::new(1.0),
+                Position::new(20.0, -140.0),
+                Velocity::new(0.0, -1.0),
+            ),
+        ])
+        .with_bonds(vec![(9, 10)])
 }

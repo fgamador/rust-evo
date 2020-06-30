d7a5651a68649c4da0b89bce2282ca9bbfc34f5a
Franz Amador <franzamador@gmail.com>
2020-06-29 19:05:27 -0700

    improve demo

--- evo_main/examples/bonded_pair.rs
+++ evo_main/examples/bonded_pair.rs
@@ -32,19 +32,19 @@ fn create_world() -> World {
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(-160.0, 50.0),
-                Velocity::new(4.0, 0.0),
+                Position::new(-160.0, 45.0),
+                Velocity::new(10.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(0.0, 50.0),
+                Position::new(0.0, 45.0),
                 Velocity::new(0.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(40.0, 50.0),
+                Position::new(40.0, 45.0),
                 Velocity::new(0.0, 0.0),
             ),
         ])
@@ -53,25 +53,25 @@ fn create_world() -> World {
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(-160.0, -50.0),
+                Position::new(-160.0, -45.0),
                 Velocity::new(4.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(0.0, -50.0),
+                Position::new(-20.0, -45.0),
                 Velocity::new(0.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(40.0, -50.0),
+                Position::new(20.0, -45.0),
                 Velocity::new(0.0, 0.0),
             ),
             Cell::ball(
                 Length::new(20.0),
                 Mass::new(1.0),
-                Position::new(80.0, -50.0),
+                Position::new(60.0, -45.0),
                 Velocity::new(0.0, 0.0),
             ),
         ])

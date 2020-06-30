eecf5302bbf8bd7c317afa24f9508c01459dd689
Franz Amador <franzamador@gmail.com>
2020-06-29 20:25:31 -0700

    improve demo

--- evo_main/examples/big_and_little.rs
+++ evo_main/examples/big_and_little.rs
@@ -1,5 +1,6 @@
 use evo_main::main_support::init_and_run;
 use evo_model::biology::cell::Cell;
+use evo_model::environment::influences::*;
 use evo_model::physics::quantities::*;
 use evo_model::world::World;
 
@@ -11,7 +12,14 @@ fn create_world() -> World {
     World::new(Position::new(-100.0, -100.0), Position::new(100.0, 100.0))
         .with_perimeter_walls()
         .with_pair_collisions()
+        .with_influence(Box::new(BondForces::new()))
         .with_cells(vec![
+            Cell::ball(
+                Length::new(5.0),
+                Mass::new(0.5),
+                Position::new(-95.0, 75.0),
+                Velocity::new(1.21, -1.0),
+            ),
             Cell::ball(
                 Length::new(40.0),
                 Mass::new(10.0),
@@ -21,8 +29,9 @@ fn create_world() -> World {
             Cell::ball(
                 Length::new(5.0),
                 Mass::new(0.5),
-                Position::new(-95.0, 75.0),
-                Velocity::new(1.21, -1.0),
+                Position::new(50.0, -5.0),
+                Velocity::new(0.0, 0.0),
             ),
         ])
+        .with_bonds(vec![(1, 2)])
 }

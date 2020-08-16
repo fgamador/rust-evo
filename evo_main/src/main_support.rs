0d2239d234626f5c3566577cc130b045c73f7e3a
Franz Amador <franzamador@gmail.com>
2020-08-16 11:57:40 -0700

    add 'seed' command-line arg

--- evo_main/src/main_support.rs
+++ evo_main/src/main_support.rs
@@ -1,5 +1,5 @@
 use crate::view::*;
-use clap::clap_app;
+use clap::{clap_app, ArgMatches};
 use evo_domain::physics::quantities::Position;
 use evo_domain::world::World;
 use evo_domain::UserAction;
@@ -9,10 +9,11 @@ use std::time::{Duration, Instant};
 pub fn init_and_run(world: World) {
     let args = parse_command_line();
     let view = View::new(world.min_corner(), world.max_corner());
-    run(world, view, args.start_paused);
+    run(world, view, args);
 }
 
 struct CommandLineArgs {
+    _seed: u64,
     start_paused: bool,
 }
 
@@ -21,19 +22,37 @@ fn parse_command_line() -> CommandLineArgs {
         (version: "0.1.0")
         (author: "Franz Amador <franzamador@gmail.com>")
         (about: "Evolution of simple digital organisms")
+        (@arg seed: -s --seed +takes_value {is_u64_arg} "Random seed")
         (@arg paused: -p --paused "Starts with the simulation paused")
     )
     .get_matches();
 
     CommandLineArgs {
+        _seed: get_u64_arg(&matches, "seed", 0),
         start_paused: matches.is_present("paused"),
     }
 }
 
-fn run(mut world: World, mut view: View, start_paused: bool) {
+fn is_u64_arg(v: String) -> Result<(), String> {
+    if v.parse::<u64>().is_ok() {
+        Ok(())
+    } else {
+        Err("Not a positive integer".to_string())
+    }
+}
+
+fn get_u64_arg(matches: &ArgMatches, name: &str, default_value: u64) -> u64 {
+    if let Some(arg) = matches.value_of(name) {
+        arg.parse::<u64>().unwrap()
+    } else {
+        default_value
+    }
+}
+
+fn run(mut world: World, mut view: View, args: CommandLineArgs) {
     view.render(&world);
 
-    let mut user_action = if start_paused {
+    let mut user_action = if args.start_paused {
         UserAction::None
     } else {
         UserAction::PlayToggle

b8e78358f7fb2c8a753f19e1b541476002ad3eb3
Franz Amador <franzamador@gmail.com>
2020-08-17 20:59:54 -0700

    r! move class

--- evo_domain/src/lib.rs
+++ evo_domain/src/lib.rs
@@ -3,6 +3,8 @@ pub mod environment;
 pub mod physics;
 pub mod world;
 
+use std::time;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum UserAction {
     DebugPrint,
@@ -11,4 +13,58 @@ pub enum UserAction {
     PlayToggle,
     SelectCellToggle { x: f64, y: f64 },
     SingleTick,
+}
+
+pub struct ElapsedTimeProbe {
+    name: &'static str,
+    reporting_interval: time::Duration,
+    next_reporting_time: time::Instant,
+    probe_begin_time: time::Instant,
+    probes_count: u32,
+    probes_duration: time::Duration,
+}
+
+impl ElapsedTimeProbe {
+    pub fn new(name: &'static str, reporting_interval: time::Duration) -> Self {
+        ElapsedTimeProbe {
+            name,
+            reporting_interval,
+            next_reporting_time: time::Instant::now() + reporting_interval,
+            probe_begin_time: time::Instant::now(),
+            probes_count: 0,
+            probes_duration: time::Duration::from_secs(0),
+        }
+    }
+
+    pub fn begin(&mut self) {
+        self.probe_begin_time = time::Instant::now();
+    }
+
+    pub fn end(&mut self) {
+        let end_time = time::Instant::now();
+        self.record_probe(end_time.duration_since(self.probe_begin_time));
+        if end_time >= self.next_reporting_time {
+            self.report();
+            self.reset();
+        }
+    }
+
+    fn record_probe(&mut self, duration: time::Duration) {
+        self.probes_count += 1;
+        self.probes_duration += duration;
+    }
+
+    fn report(&self) {
+        println!(
+            "{}: {:?}",
+            self.name,
+            self.probes_duration / self.probes_count
+        );
+    }
+
+    fn reset(&mut self) {
+        self.probes_count = 0;
+        self.probes_duration = time::Duration::from_secs(0);
+        self.next_reporting_time = time::Instant::now() + self.reporting_interval;
+    }
 }
--- evo_domain/src/world.rs
+++ evo_domain/src/world.rs
@@ -4,6 +4,7 @@ use crate::environment::influences::*;
 use crate::physics::bond::*;
 use crate::physics::quantities::*;
 use crate::physics::sortable_graph::*;
+use crate::ElapsedTimeProbe;
 use rayon::prelude::*;
 use std::collections::HashSet;
 use std::iter::FromIterator;
@@ -346,60 +347,6 @@ struct NewChildData {
     parent: NodeHandle,
     bond_index: usize,
     child: Cell,
-}
-
-pub struct ElapsedTimeProbe {
-    name: &'static str,
-    reporting_interval: time::Duration,
-    next_reporting_time: time::Instant,
-    probe_begin_time: time::Instant,
-    probes_count: u32,
-    probes_duration: time::Duration,
-}
-
-impl ElapsedTimeProbe {
-    pub fn new(name: &'static str, reporting_interval: time::Duration) -> Self {
-        ElapsedTimeProbe {
-            name,
-            reporting_interval,
-            next_reporting_time: time::Instant::now() + reporting_interval,
-            probe_begin_time: time::Instant::now(),
-            probes_count: 0,
-            probes_duration: time::Duration::from_secs(0),
-        }
-    }
-
-    pub fn begin(&mut self) {
-        self.probe_begin_time = time::Instant::now();
-    }
-
-    pub fn end(&mut self) {
-        let end_time = time::Instant::now();
-        self.record_probe(end_time.duration_since(self.probe_begin_time));
-        if end_time >= self.next_reporting_time {
-            self.report();
-            self.reset();
-        }
-    }
-
-    fn record_probe(&mut self, duration: time::Duration) {
-        self.probes_count += 1;
-        self.probes_duration += duration;
-    }
-
-    fn report(&self) {
-        println!(
-            "{}: {:?}",
-            self.name,
-            self.probes_duration / self.probes_count
-        );
-    }
-
-    fn reset(&mut self) {
-        self.probes_count = 0;
-        self.probes_duration = time::Duration::from_secs(0);
-        self.next_reporting_time = time::Instant::now() + self.reporting_interval;
-    }
 }
 
 #[cfg(test)]
